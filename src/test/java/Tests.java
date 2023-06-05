0c1f19895baac2e57a4a5bad676857ecbfe89d5d
Artem Perestoronin <aperestoronin@aicorner.org>
2023-06-05 20:20:46 +0200

    new tests

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -14,4 +14,41 @@ public class Tests {
 
         assertEquals(-1, calculator.calculate(expression));
     }
+
+    @Test
+    public void longNumbersTest() {
+        Calculator calculator = new Calculator();
+
+        String expression = "(-15000000000 + 10) / 5";
+
+        assertEquals(-2999999998L, calculator.calculate(expression));
+    }
+
+    @Test
+    public void floatNumbersTest() {
+        Calculator calculator = new Calculator();
+
+        String expression = "(-15.5 + 10) / 5";
+
+        assertEquals(-1.1, calculator.calculate(expression));
+    }
+
+    @Test
+    public void doubleBracketTest() {
+        Calculator calculator = new Calculator();
+
+        String expression = "((-10 -5 + 10)) / 5";
+
+        assertEquals(-1, calculator.calculate(expression));
+    }
+
+    @Test
+    public void doubleBracket2Test() {
+        Calculator calculator = new Calculator();
+
+        String expression = "(-15 + 10) / (5+5)";
+
+        assertEquals(-0.5d, calculator.calculate(expression));
+    }
+
 }
